de2294c5a500d32ef22a986cd988c657f1abf348
nedulau <27210700-nedulau@users.noreply.gitlab.com>
2025-11-24 13:46:15 +0100

    Aufgabe 6.1 1. Punkt

--- domain/src/main/java/de/seuhd/campuscoffee/domain/model/User.java
+++ domain/src/main/java/de/seuhd/campuscoffee/domain/model/User.java
@@ -1,13 +1,26 @@
 package de.seuhd.campuscoffee.domain.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Builder(toBuilder = true)
 public record User (
-        //TODO: Implement user domain object
+        @Nullable Long id, // id is null when creating a new user
+        @Nullable LocalDateTime createdAt, // is null when using DTO to create a new user
+        @Nullable LocalDateTime updatedAt, // is set when creating or updating a user
+        @NonNull String loginName,
+        @NonNull String emailAddress,
+        @NonNull String firstName,
+        @NonNull String lastName
 ) implements Serializable { // serializable to allow cloning (see TestFixtures class).
     @Serial
     private static final long serialVersionUID = 1L;
